0851a565c10cb47089005498320811599bbf7220
Nikhil Shashidhar <e0725992@u.nus.edu>
2023-03-07 18:32:14 +0800

    Refactor Help JUnit Tests

--- src/main/java/seedu/clialgo/Ui.java
+++ src/main/java/seedu/clialgo/Ui.java
@@ -64,7 +64,7 @@ public class Ui {
                         "[list]: displays all notes\n" +
                         "[filter]: filters notes by topic\n" +
                         "[exit]: close the application\n" +
-                        "For more help on a specific command, type `help c/COMMAND_TYPE`\n");
+                        "For more help on a specific command, type `help c/COMMAND_TYPE`");
         printDivider();
     }
 
@@ -129,6 +129,7 @@ public class Ui {
         printDivider();
         System.out.println("Unsuccessful!\n" +
                         "Type 'help c/remove' for assistance.");
+        printDivider();
     }
 
     /**
