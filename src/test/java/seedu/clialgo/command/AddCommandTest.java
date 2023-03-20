e09d352f9bc5e87e5b5102490cbccc773c5f9e1d
nicholas132000 <beefnlemon@gmail.com>
2023-03-21 03:06:31 +0800

    Refactor to checkstyle

--- src/test/java/seedu/clialgo/command/AddCommandTest.java
+++ src/test/java/seedu/clialgo/command/AddCommandTest.java
@@ -7,11 +7,9 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
 
-import org.w3c.dom.ls.LSOutput;
 import seedu.clialgo.TopicManager;
 import seedu.clialgo.Ui;
 import seedu.clialgo.storage.FileManager;
-import seedu.clialgo.storage.SingleFile;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
