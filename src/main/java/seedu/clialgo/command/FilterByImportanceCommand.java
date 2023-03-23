b88cb2a3e8335d9278213893169b34cab88fa143
Nikhil Shashidhar <e0725992@u.nus.edu>
2023-03-23 17:05:13 +0800

    Refactor inherited FilterCommand class

--- src/main/java/seedu/clialgo/command/FilterByImportanceCommand.java
+++ src/main/java/seedu/clialgo/command/FilterByImportanceCommand.java
@@ -1,7 +1,38 @@
 package seedu.clialgo.command;
 
+import seedu.clialgo.Buffer;
+import seedu.clialgo.Topic;
+import seedu.clialgo.TopicManager;
+import seedu.clialgo.Ui;
+import seedu.clialgo.file.CS2040CFile;
+import seedu.clialgo.storage.FileManager;
+
+import java.util.ArrayList;
+
 public class FilterByImportanceCommand extends FilterCommand {
     public FilterByImportanceCommand(String keyWord, String topic) {
         super(keyWord, topic);
+    }
+
+    @Override
+    public void execute(TopicManager topicManager, Ui ui, FileManager fileManager, Buffer buffer) {
+        if (this.topic == null) {
+            printAllTopics(topicManager, ui);
+            buffer.updateBuffer(topicManager.getAllFilesAsFiles());
+            return;
+        }
+        if (!topicManager.isValidTopic(this.topic)) {
+            new InvalidTopicCommand(this.topic).execute(topicManager, ui, fileManager, buffer);
+            return;
+        }
+        if (topicManager.isTopicEmpty(this.topic)) {
+            ui.printFilterFail();
+            buffer.updateBuffer(new ArrayList<>());
+            return;
+        }
+        printSingleTopic(topicManager, ui);
+        Topic topic = topicManager.getOneTopic(this.topic);
+        ArrayList<CS2040CFile> files = topic.getCS2040CFilesAsArray();
+        buffer.updateBuffer(files);
     }
 }
