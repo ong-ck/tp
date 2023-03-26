b134f010f74fe78a7c0cff1db5791a67c2d1f531
heejet <heejet@gmail.com>
2023-03-26 17:14:01 +0800

    Add CLIAlgo support for empty buffer message

--- src/main/java/seedu/clialgo/command/ExportCommand.java
+++ src/main/java/seedu/clialgo/command/ExportCommand.java
@@ -13,9 +13,13 @@ public class ExportCommand extends Command {
      * @param topicManager The <code>TopicManager</code> object which handles all CS2040CFiles stored in CLIAlgo.
      * @param ui The <code>Ui</code> object which handles outputs to the user.
      * @param fileManager The <code>FileManager</code> object responsible for saving information in CLIAlgo.
-     * @param buffer The object responsible to export filtered files.
+     * @param buffer The <code>Buffer</code> object responsible to export filtered files.
      */
     public void execute (TopicManager topicManager, Ui ui, FileManager fileManager, Buffer buffer) {
+        if (buffer.isEmpty()) {
+            new EmptyBufferCommand().execute(topicManager, ui, fileManager, buffer);
+            return;
+        }
         buffer.exportBuffer();
     }
 
