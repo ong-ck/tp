7f8f1a3c2831f516b6932594454a99bb3ab41fb8
JooHoe <e0725456@u.nus.edu>
2023-04-04 18:33:20 +0800

    Fix: checkstyle

--- src/main/java/seedu/clialgo/Buffer.java
+++ src/main/java/seedu/clialgo/Buffer.java
@@ -2,7 +2,7 @@ package seedu.clialgo;
 
 import seedu.clialgo.file.CS2040CFile;
 
-import java.awt.*;
+import java.awt.HeadlessException;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
