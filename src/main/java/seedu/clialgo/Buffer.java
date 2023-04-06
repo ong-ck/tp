9cac920848f87683e60df7fa2bc6469884b72b6e
ong-ck <e0774208@u.nus.edu>
2023-04-06 15:04:12 +0800

    Fix checkstyle warning in Buffer

--- src/main/java/seedu/clialgo/Buffer.java
+++ src/main/java/seedu/clialgo/Buffer.java
@@ -19,10 +19,10 @@ import java.util.ArrayList;
 public class Buffer {
 
     private static Buffer buffer = null;
+    private static final String CURRENT_DIRECTORY_PATH = ".\\";
     private final ArrayList<CS2040CFile> files;
     private final File pathToBuffer;
     private final Ui ui;
-    private final String CURRENT_DIRECTORY_PATH = ".\\";
 
 
     /**
